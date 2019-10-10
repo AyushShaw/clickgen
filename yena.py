4e40e3d4f0d9426d5e5f4f9c30675bb8f04a5c48
Ayush Shaw <ayushshaw17@gmail.com>
2019-10-10 07:58:13 +0530

    Updated File check systems,

--- yena.py
+++ yena.py
@@ -8,49 +8,87 @@ def curate(lst, thing):
                 lst.append(thing)
         return lst
 
+
+intro_txt ="""
+     _/_/_/  _/  _/            _/          _/_/_/                     
+ _/        _/        _/_/_/  _/  _/    _/          _/_/    _/_/_/    
+_/        _/  _/  _/        _/_/      _/  _/_/  _/_/_/_/  _/    _/   
+_/        _/  _/  _/        _/  _/    _/    _/  _/        _/    _/    
+ _/_/_/  _/  _/    _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/    
+-----------------------------------------------------------------
+
+Click is a Python package for creating beautiful command line interfaces
+in a composable way with as little code as necessary. It's the "Command
+Line Interface Creation Kit". It's highly configurable but comes with 
+sensible defaults out of the box.
+
+ClickGen is a Command line Application that helps in curating a Click
+development directory/enviornment. 
+                
+                
+                
+                """
+click.echo(intro_txt)
 @click.command()
+
 @click.option('--name', prompt ='Application Name', default ='Ap1',
                 help ='The Name of the Application')
 @click.option('--version', prompt ='Application Version', default ='1.0',
                 help ='The Version of the Application')
-@click.option('--pyModules', prompt ='Modules in Application', default ='main',
+@click.option('--pymods', prompt ='Modules in Application', default ='main',
                 help ='The Name of modules to be used by the Application')
 @click.option('--requires', prompt ='Packages required (other that click)', default ='Click',
                 help ='The Name of the python packages required for this  Application')
-
-
-def cli(name, version, pyModules, requires):
+def cli(name, version, pymods, requires):
         """
-                                                                              
-     _/_/_/  _/  _/            _/          _/_/_/                     
-  _/        _/        _/_/_/  _/  _/    _/          _/_/    _/_/_/    
- _/        _/  _/  _/        _/_/      _/  _/_/  _/_/_/_/  _/    _/   
-_/        _/  _/  _/        _/  _/    _/    _/  _/        _/    _/    
- _/_/_/  _/  _/    _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/     
-                                                                      
-
-
         Generates Basic Click development Enviornment
-   
+        ----------------------------------------------
         
         """
+        
         click.echo(" (1/3) Processing Input Data.. ")
-        pyModules = curate(pyModules, 'main')
+        pymods = curate(pymods, 'main')
         requires = curate(requires, 'Click')
 
 
         click.echo(" (2/3) Brewing up Files and Directories.. ")
-        os.mkdir(name)
         rootfolder = os.getcwd() + '/' + name
-        Path(rootfolder + '/setup.py').touch
-        for modules in pyModules:
-                Path(rootfolder + '/' + modules + '.py').touch
+        if Path(rootfolder).is_dir():
+                click.echo('The directory named {} is already there!!'.format(name))
+        else:
+                os.mkdir(name)
+        
+        if Path(rootfolder+'/setup.py').is_file():
+                click.echo('setup.py is already there!!')
+        else:
+                Path(rootfolder + '/setup.py').touch()
+        for modules in pymods:
+                if Path(rootfolder + '/' + modules + '.py').is_file():
+                        click.echo('{}.py is already there!!'.format(modules))
+                else:
+                        Path(rootfolder + '/' + modules + '.py').touch()
 
 
         click.echo(" (3/3) Adding Mints and scripts for smooth development..")
-        setup_txt = Path('setup.txt')
-        txt = setup_txt.read_text()
-        txt = txt.format(name,version,str(pyModules),str(requires),name)
+        set
+        txt =   """
+from setuptools import setup
+
+setup(
+        name={},
+        version={},
+        py_modules={},
+        install_requires={},
+        entry_points='''
+        [console_scripts]
+        {}=main:cli
+        '''
+)
+
+                """
+
+
+        txt = txt.format(str(name),str(version),str(pymods),str(requires),str(name))
         setup_py = Path(rootfolder + '/setup.py')
         setup_py.write_text(txt)
 
